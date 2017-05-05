fe05361b5a836b34996fd34323a77dd25a1a4641
jianhuang <jianhuang0@gmail.com>
2017-05-05 17:11:57 +0800

    annote part of the ngx_string.h code

--- src/core/ngx_string.h
+++ src/core/ngx_string.h
@@ -13,8 +13,13 @@
 
 
 typedef struct {
+
+    //字符串的长度
     size_t      len;
+
+    //字符串指针
     u_char     *data;
+
 } ngx_str_t;
 
 
@@ -38,20 +43,24 @@ typedef struct {
 #define ngx_string(str)     { sizeof(str) - 1, (u_char *) str }
 #define ngx_null_string     { 0, NULL }
 
-
+//大小写变换
 #define ngx_tolower(c)      (u_char) ((c >= 'A' && c <= 'Z') ? (c | 0x20) : c)
 #define ngx_toupper(c)      (u_char) ((c >= 'a' && c <= 'z') ? (c & ~0x20) : c)
 
-
+//字符串比较
 #define ngx_strncmp(s1, s2, n)  strncmp((const char *) s1, (const char *) s2, n)
 
 
 /* msvc and icc7 compile strcmp() to inline loop */
 #define ngx_strcmp(s1, s2)  strcmp((const char *) s1, (const char *) s2)
 
-
+//字符串s1中第一次出现子串s2的指针
 #define ngx_strstr(s1, s2)  strstr((const char *) s1, (const char *) s2)
+
+//字符串s1中第一次出现字符c的指针
 #define ngx_strchr(s1, c)   strchr((const char *) s1, (int) c)
+
+//字符串长度
 #define ngx_strlen(s)       strlen((const char *) s)
 
 
@@ -60,6 +69,8 @@ typedef struct {
  * while ZeroMemory() and bzero() are the calls.
  * icc7 may also inline several mov's of a zeroed register for small blocks.
  */
+
+//初始化字符串
 #define ngx_memzero(buf, n)       (void) memset(buf, 0, n)
 #define ngx_memset(buf, c, n)     (void) memset(buf, c, n)
 
@@ -89,6 +100,7 @@ void *ngx_memcpy(void *dst, void *src, size_t n);
  * bytes faster than icc8 autodetecting _intel_fast_memcpy()
  */
 
+//字符串复制指定长度len
 static ngx_inline u_char *
 ngx_copy(u_char *dst, u_char *src, size_t len)
 {
@@ -117,39 +129,52 @@ ngx_copy(u_char *dst, u_char *src, size_t len)
 #define ngx_memcmp                memcmp
 
 
+//字符串复制
 u_char *ngx_cpystrn(u_char *dst, u_char *src, size_t n);
+//复制ngx_str_t data数据返回新的字符串指针
 u_char *ngx_pstrdup(ngx_pool_t *pool, ngx_str_t *src);
+
+//格式化输出
 u_char * ngx_cdecl ngx_sprintf(u_char *buf, const char *fmt, ...);
 u_char * ngx_cdecl ngx_snprintf(u_char *buf, size_t max, const char *fmt, ...);
 u_char *ngx_vsnprintf(u_char *buf, size_t max, const char *fmt, va_list args);
 
+//比较A-Z 和 a-z的字符组成的字符串
+//不区分大小写
 ngx_int_t ngx_strcasecmp(u_char *s1, u_char *s2);
 ngx_int_t ngx_strncasecmp(u_char *s1, u_char *s2, size_t n);
 
 u_char *ngx_strnstr(u_char *s1, char *s2, size_t n);
 
+//匹配子串
 u_char *ngx_strstrn(u_char *s1, char *s2, size_t n);
 u_char *ngx_strcasestrn(u_char *s1, char *s2, size_t n);
 
+//反向比较字符串
 ngx_int_t ngx_rstrncmp(u_char *s1, u_char *s2, size_t n);
 ngx_int_t ngx_rstrncasecmp(u_char *s1, u_char *s2, size_t n);
+
 ngx_int_t ngx_memn2cmp(u_char *s1, u_char *s2, size_t n1, size_t n2);
 
+//字符串转数值
 ngx_int_t ngx_atoi(u_char *line, size_t n);
 ssize_t ngx_atosz(u_char *line, size_t n);
 off_t ngx_atoof(u_char *line, size_t n);
 time_t ngx_atotm(u_char *line, size_t n);
 ngx_int_t ngx_hextoi(u_char *line, size_t n);
 
+//字符串算MD5
 void ngx_md5_text(u_char *text, u_char *md5);
 
 
 #define ngx_base64_encoded_length(len)  (((len + 2) / 3) * 4)
 #define ngx_base64_decoded_length(len)  (((len + 3) / 4) * 3)
 
+//字符串base64
 void ngx_encode_base64(ngx_str_t *dst, ngx_str_t *src);
 ngx_int_t ngx_decode_base64(ngx_str_t *dst, ngx_str_t *src);
 
+//decode 多个字符串
 uint32_t ngx_utf_decode(u_char **p, size_t n);
 size_t ngx_utf_length(u_char *p, size_t n);
 u_char *ngx_utf_cpystrn(u_char *dst, u_char *src, size_t n);
